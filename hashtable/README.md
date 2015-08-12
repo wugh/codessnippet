c20a348a317c35d2262e8e9ed48c8f87dcb2f06f
guohuawu <trustghwu@gmail.com>
2015-08-12 20:03:30 +0800

    add hash table

---
+++ hashtable/README.md
@@ -0,0 +1,8 @@
+# Hashtable implement by CPP
+A simple hashtable implement, solve collisions by chaining.
+
+# TODO
+1. Find better hash fuction.
+2. Sort chaining list.
+3. Chaining with a BST.
+
---
+++ hashtable/hashtable.cpp
@@ -0,0 +1,116 @@
+#include<string>
+#include<vector>
+#include<climits>
+#include<iostream>
+
+struct Entry {
+  std::string key;
+  std::string value;
+  Entry* next;
+  Entry(const std::string &k, const std::string &v): key(k), value(v), next(NULL){}
+};
+
+
+class HashTable {
+ private:
+  int size;
+  std::vector<Entry*> table;
+
+ public:
+  HashTable(int n): size(n) {
+    table.reserve(n);
+    for (int i = 0; i < size; i++)
+      table.push_back(NULL);
+  }
+
+  /**
+   * hash string to int
+   * refer to
+   * https://stackoverflow.com/questions/98153/whats-the-best-hashing-algorithm-to-use-on-a-stl-string-when-using-hash-map
+   */
+  int hash(const std::string &key) {
+    unsigned long int hashval = 0;
+    int i = 0;
+
+    // string to integer
+    while (i < key.length()) {
+      hashval = hashval * 101 + key[i++];
+    }
+    return hashval % size;
+  }
+
+  void put(const std::string &key, const std::string &value) {
+    int index = hash(key);
+    std::cout << key << " index is " << index << std::endl;
+    
+    // try to find key and move cur ptr to end of chain
+    Entry *cur = table[index];
+    if (cur == NULL) {
+      Entry *ptr = new Entry(key, value);
+      // the chain is empty
+      table[index] = ptr;
+    }
+
+    while (cur != NULL && cur->key != key) {
+      cur = cur->next;
+    }
+
+    if (cur != NULL) {
+      // find it
+      cur->value = value;
+    } else {
+      // can not find it, then insert it to the end of chain
+      Entry *ptr = new Entry(key, value);
+      ptr->next = table[index]->next;
+      table[index]->next = ptr;
+    }
+  }
+
+  int get(const std::string &key, std::string &value) {
+    int index = hash(key);
+    Entry *cur = table[index];
+    while (cur != NULL && cur->key != key) {
+      cur = cur->next;
+    }
+    if (cur == NULL) {
+      return -1;
+    } else {
+      value = cur->value;
+      return 0;
+    }
+  }
+};
+
+
+void print(HashTable &hashtable, const std::string &key) {
+  std::string value;
+  if (hashtable.get(key, value) == 0) {
+    std::cout << key << " => " << value << std::endl;
+  } else {
+    std::cout << "can not find key: " << key << std::endl;
+  }
+}
+
+int main() {
+
+  HashTable hashtable(65536);
+  
+  hashtable.put("key1", "value1");
+  hashtable.put("key2", "value2");
+  hashtable.put("key3", "value3");
+  hashtable.put("key4", "value4");
+  print(hashtable, "key1");
+  print(hashtable, "key2");
+  print(hashtable, "key3");
+  print(hashtable, "key4");
+
+  // modify value
+  hashtable.put("key1", "value9");
+
+  print(hashtable, "key1");
+  print(hashtable, "key2");
+  print(hashtable, "key3");
+  print(hashtable, "key4");
+
+	return 0;
+}
